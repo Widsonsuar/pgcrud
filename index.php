526372fe5a7bbcff3dc124a7c512f682e9d7ff3c
ADELSO BAUDILIO HERNANDEZ GONZALES <147952990+adelsoumg@users.noreply.github.com>
2023-10-28 15:53:46 -0600

    Add files via upload
    Menu Principal con CSS

--- index.php
+++ index.php
@@ -1,75 +1,99 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Gestión de Empleados</title>
+    <title>Menu Principal</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
         }
 
-        h1 {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        /* Estilos para el contenedor principal */
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        /* Estilos para el contenedor de botones */
-        .button-container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        /* Estilos para los botones */
-        .btn {
-            padding: 10px 20px;
-            background-color: #007bff;
+        header {
+            background-color: #333;
             color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+
+        nav {
+            background-color: #444;
+            text-align: center;
+        }
+
+        nav ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        nav li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav a {
             text-decoration: none;
-            border: none;
-            cursor: pointer;
-            margin: 10px;
+            color: #fff;
+            font-size: 18px;
+            padding: 10px 20px;
             border-radius: 5px;
         }
 
-        .btn-primary {
-            background-color: #007bff;
+        nav a:hover {
+            background-color: #555;
         }
 
-        .btn-success {
-            background-color: #28a745;
+        .container {
+            max-width: 1297px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-size: 24px;
+            text-align: center;
+        }
+
+        p {
+            font-size: 16px;
+            text-align: center;
+        }
+
+        .footer {
+            text-align: center;
+            background-color: #333;
+            color: #fff;
+            padding: 20px 0;
+        }
+
+        .footer img {
+            width: 1000px; /* Ajusta el tamaño de la imagen según tus necesidades */
         }
     </style>
 </head>
 <body>
-    <h1>Gestión de Empleados</h1>
-
-    <!-- Contenedor principal -->
+    <header>
+        <h1>Menu Principal</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="agregar.php">Agregar un Empleado</a></li>
+            <li><a href="listar.php">Empleados</a></li>
+            <li><a href="proveedores.php">Proveedores</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+    </nav>
     <div class="container">
-        <!-- Columna izquierda para el botón "Insertar Empleado" -->
-        <div class="button-container">
-            <!-- Botón para redireccionar a la página de inserción -->
-            <form action="agregar.php" method="GET">
-                <button type="submit" class="btn btn-primary">Insertar Empleado</button>
-            </form>
-        </div>
-
-        <!-- Columna derecha para el botón "Listar Empleados" -->
-        <div class="button-container">
-            <!-- Botón para redireccionar a la página de listado -->
-            <form action="listar.php" method="GET">
-                <button type="submit" class="btn btn-success">Listar Empleados</button>
-            </form>
-        </div>
+        <h1>Bienvenido al Menu Principal</h1>
+        <p>Seleccione una opcion del menu.</p>
+    </div>
+    <ul>
+    </ul>
+    <div class="footer">
+        <img src="empleados.png" alt="foto">
     </div>
 </body>
-</html>
